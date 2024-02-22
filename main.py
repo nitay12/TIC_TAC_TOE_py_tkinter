64ef1ae4f0f57f2d4624add2c1928a3cbf2d4345
Nitay Caspi <NitayCaspi12@gmail.com>
2024-02-22 13:27:45 +0200

    init project

---
+++ main.py
@@ -0,0 +1,20 @@
+current_player = "X"
+
+
+def start():
+    # Open game window
+    # Start the game
+    pass
+
+
+def on_button_pressed():
+    # switch the button label to the current player shape
+    # swtich current player to the other shape
+    # check_winning()
+    # check_draw()
+    pass
+
+
+def check_winning():
+    # check if all the rows\columns\slants are in the same shape
+    pass
